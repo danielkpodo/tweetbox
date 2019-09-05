640d567a99caea72d7541207cd91fe40cc9509fe
naphtha <naphthanewman@gmail.com>
2019-09-05 09:48:38 +0000

    generated tweet data

---
+++ src/tweetData.jsx
@@ -0,0 +1,13 @@
+const testTweet = {
+  message: "Everybody in this country needs to learn to program",
+  gravatar: "999a5c9a1cf4718be5f393ea94723f1f",
+  author: {
+    handle: "@narhKpodo",
+    name: "Naphtha Newman"
+  },
+  likes: 5,
+  retweets: 2,
+  timestamp: "2019-04-06 21:24:37"
+};
+
+export default testTweet;
